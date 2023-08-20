ba09a7847c2207abcdbafa2ff4d2362e529ba738
R8V <ignacio.slater@ug.uchile.cl>
2023-08-20 11:46:55 -0400

    Remove obsolete network examples and related functions
    Deleted two python files (feed_forward_mnist.py and gradient_checking.py) from the src/examples/networks directory and a function (evaluate_network) from the networks package. These models and functions were used in the early stages of the project for testing and demonstrating various functionalities, but they are no longer relevant to the current structure and aim of the project. Removed these files and functions to avoid confusion and maintain project cleanliness.

--- src/networks/__init__.py
+++ src/networks/__init__.py
@@ -1,12 +1,10 @@
 # Preceding underscore to assure that this is imported first
 from ._neural_network import NeuralNetwork
-from .evaluation import evaluate_network
 from .feed_forward_network import FeedForwardNetwork
 
 __all__ = [
     "FeedForwardNetwork",
     "NeuralNetwork",
-    "evaluate_network",
 ]
 
 __author__ = ["Roäc Ravenhill"]
